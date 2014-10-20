97b46847ac78b3c9c181ba8a35a442602724d9dc
Martin Andersson-Tenninge <martin.andersson-tenninge@yh.nackademin.se>
2014-10-20 10:08:16 +0200

    unit tests färdiga

--- Tentamen/CrimeHeadlineUnitTests/CrimeHeadlineTests.cs
+++ Tentamen/CrimeHeadlineUnitTests/CrimeHeadlineTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
 
 namespace CrimeHeadlineUnitTests
 {
@@ -7,12 +8,29 @@ namespace CrimeHeadlineUnitTests
     public class CrimeHeadlineTests
     {
         [TestMethod]
-        public void Test_That_Screen_Scraper_Returns_A_Value()
+        public void Test_That_Screen_Scraper_Polisen_Returns_A_Value()
         {
             //Arrange
-
+            string expected = "fyll i rubrik här på senaste nyheten";
+            ScreenScraperPolisen scrPolisen = new ScreenScraperPolisen();
             //Act
+            Task<string> scrPolisenTask = scrPolisen.ScreenScrapeAsync();
+            string actual = scrPolisenTask.Result;
             //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_That_Screen_Scraper_Utryckning_Returns_A_Value()
+        {
+            //Arrange
+            string expected = "fyll i rubrik här på senaste nyheten";
+            ScreenScraperUtryckning scrUtryckning = new ScreenScraperUtryckning();
+            //Act
+            Task<string> scrUtryckning = scrUtryckning.ScreenScrapeAsync();
+            string actual = scrUtryckning.Result;
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
