c03d327a5aa9fc983ef75fc31ac6a5ddafb63681
Martin Andersson-Tenninge <martin.andersson-tenninge@yh.nackademin.se>
2014-10-20 10:00:17 +0200

    Added unit test project

---
+++ Tentamen/CrimeHeadlineUnitTests/CrimeHeadlineTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrimeHeadlineUnitTests
+{
+    [TestClass]
+    public class CrimeHeadlineTests
+    {
+        [TestMethod]
+        public void Test_That_Screen_Scraper_Returns_A_Value()
+        {
+            //Arrange
+
+            //Act
+            //Assert
+        }
+    }
+}
